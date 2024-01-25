31cf61787fc3af8632bf364fda247c5d721e05b4
gsolis7 <grsolis@uiowa.edu>
2024-01-25 13:43:22 -0600

    echo.py is complete

--- echo.py
+++ echo.py
@@ -1,1 +1,13 @@
-#test commit
+def echo(text: str, repetitions: int = 3) -> str:
+    """Imitate a real-world echo."""
+    for i in range(3,0, -1):
+        if len(text) - i <= 0:
+            pass
+        else:
+            print(text[-i:])
+    print(".")
+
+
+if __name__ == "__main__":
+    text = input("Yell something at a mountain: ")
+    print(echo(text))
